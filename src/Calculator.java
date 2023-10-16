eec1f2fb1699e0a7f1c574892e5d6ddae46a691d
mihailkostromin <kostrominmi@mail.ru>
2023-10-16 22:00:20 +0300

    Change Calculator code

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,21 +1,23 @@
 public class Calculator {
-    public static <T extends Number> double sum(T a, T b) {
+    public static <T extends Number, N extends Number> double sum(T a, N b) {
         return a.doubleValue() + b.doubleValue();
     }
 
-    public static <T extends Number> double subtract(T a, T b) {
+    public static <T extends Number, N extends Number> double subtract(T a, N b) {
         return a.doubleValue() - b.doubleValue();
     }
 
-    public static <T extends Number> double multiply(T a, T b) {
+    public static <T extends Number,N extends Number> double multiply(T a, N b) {
         return a.doubleValue() * b.doubleValue();
     }
-    public static <T extends Number> double divide(T a, T b) {
-        if (b.doubleValue()==0){
+
+    public static <T extends Number, N extends Number> double divide(T a, N b) {
+        if (b.doubleValue() == 0) {
             throw new ArithmeticException("Division by zero");
         }
         return a.doubleValue() / b.doubleValue();
     }
+
     public static void main(String[] args) {
         System.out.println("Sum: " + Calculator.sum(5, 3.5));          // Выводит: Sum: 8.5
         System.out.println("Multiply: " + Calculator.multiply(4, 2.5)); // Выводит: Multiply: 10.0
